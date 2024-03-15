6abb3a120927c250afd18dbb35ec55952d4b4f0c
JBarrett4777 <160150575+JBarrett4777@users.noreply.github.com>
2024-03-15 13:55:47 +0000

    fix code

--- P3HW1_Debug.py
+++ P3HW1_Debug.py
@@ -3,42 +3,41 @@
 #P3HW1
 #Fixing code
 
-
 # This program takes a number grade , determines average and displays letter grade for average.
 
 # Enter grades for six modules
 
 mod_1 = input('Enter grade for Module 1: ')
- mod_2 = input('Enter grade for Module 2: ')
-mod_1 = input('Enter grade for Module 3: )
-mod 4 = input('Enter grade for Module 1: ')
-mod_1 = input('Enter grade for Module 5: ')
- mod_1 = input('Enter grade for Module 6: ")
+mod_2 = input('Enter grade for Module 2: ')
+mod_3 = input('Enter grade for Module 3: ')
+mod_4 = input('Enter grade for Module 4: ')
+mod_5 = input('Enter grade for Module 5: ')
+mod_6 = input('Enter grade for Module 6: ')
 
 # add grades entered to a list
 
-grades = [mod_1 mod2, mod_3, mod_4, Mod_5]
+grades = [mod_1, mod_2, mod_3, mod_4, mod_5, mod_6]
 # TO DO: determine lowest, highest , sum and average for grades
 
-low = min(Grades)
-high = high(grades)
-sum = sum(grades)
-avg =
+grade_min = min(grades)
+grade_max = max(grades)
+grade_sum = sum(grades)
+
+grade_avg = 
 
 # determine letter grade for average
 
 
 if avg >= 90:
-print('Your grade is: A')
+ print('Your grade is: A')
 else:
-if average > 80:
+ print()
+if avg > 80:
  print('Your grade is: B')
 else:
+ print('')
+if
+print
 
 else:
-print('Your grade is: F') # TO DO: finish this
-
-
-
-
-
+ print('Your grade is: F') #TO DO: Finish this
