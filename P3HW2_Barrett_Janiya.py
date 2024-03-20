e5f9b1231585ce8d6e416c1290bf59fa34fde412
JBarrett4777 <160150575+JBarrett4777@users.noreply.github.com>
2024-03-20 18:22:53 +0000

    Branching

--- P3HW2_Barrett_Janiya.py
+++ P3HW2_Barrett_Janiya.py
@@ -11,22 +11,30 @@ If yes, calculate the amount owed to employee for overtime hours
 Calculate amount employee should be paid for regular hours worked.
 Display gross pay 
 '''
-
+#Variables
 name = input("Enter employee's name: ")
 hours = float(input("Enter number of hours worked: "))
 pay_rate = float(input("Enter employee's pay rate: "))
 overtime = (hours - 40)
+reg_hours = (hours - overtime)
+#finish this!!!!! (Probably finished it already!)
+overtime_pay = (overtime * pay_rate)
+reg_pay = (reg_hours * pay_rate)
+gross_pay = (reg_pay + overtime_pay)
+txt1 = "Hours Worked"
+txt2 = "Pay Rate"
+txt3 = "OverTime"
+txt4 = "OverTime Pay"
+txt5 = "RegHour Pay"
+txt6 = "Gross Pay"
 
 print("----------------------------------------")
 print(f"Employee name: {name}")
 print()
-print(f'Hours Worked {hours: <15}')
-if hours > 40:
-    print(f"OverTime {overtime}")
-else:
-    print("None")
-'''
-#figure out the formatting
-print("Hours Worked    Overtime Pay")
-print(f'{hours:<15}{overtime}')
-'''
+
+print(f'{txt1:<15}{txt2:<10}{txt3:<10}{txt4:<15}{txt5:<15}{txt6:<15}')
+print("--------------------------------------------------------------------------------")
+#FINISH OUTPUT!!!!
+print(f'{hours:<15}')
+
+
